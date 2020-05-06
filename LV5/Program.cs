9eafa62ebcbfe7ef125e79195c09f06a1819fe09
Luka1495 <62608325+Luka1495@users.noreply.github.com>
2020-05-06 16:48:38 +0200

    Add files via upload

---
+++ LV5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box("boxing day");
+            Product product = new Product("Luka", 69, 4);
+            Product pro = new Product("Vukadin", 33, 3);
+            box.Add(product);
+            box.Add(pro);
+
+
+            ShippingService price = new ShippingService(4);
+            double w = 0;
+
+            w += box.Weight;
+            Console.WriteLine(box.Description());
+
+
+            Console.WriteLine(price.ToString() + price.Price(w) + " kuna");
+
+        }
+    }
+}
---
+++ LV5/ShippingService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV5
+{
+    class ShippingService
+    {
+        private double JedincaMase;
+
+        public ShippingService(double JedincaMase) { this.JedincaMase = JedincaMase; }
+
+        public double Price(double weight)
+        {
+            return JedincaMase * weight;
+        }
+
+        public override string ToString()
+        {
+            return "Ukupna cijena dostave je:";
+        }
+    }
+}
